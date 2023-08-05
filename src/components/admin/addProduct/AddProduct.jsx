57cd49fd98dedb5e8ef5b687360b01afa5e92740
Huski-commando <gyan.mu@gmail.com>
2023-08-05 21:11:56 +0530

    finished add product section and will start working on getting products from firestore

--- src/components/admin/addProduct/AddProduct.jsx
+++ src/components/admin/addProduct/AddProduct.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import styles from "./AddProduct.module.scss";
 import Card from "../../card/Card";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { db, storage } from "../../../firebase/config";
+import { toast } from "react-toastify";
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../loader/Loader";
 
 const categories = [
   { id: 1, name: "Laptop" },
@@ -9,114 +15,206 @@ const categories = [
   { id: 4, name: "Fashion" },
 ];
 
+const initialState = {
+  name: "",
+  imageURL: "",
+  price: 0,
+  category: "",
+  brand: "",
+  desc: "",
+};
+
 const AddProduct = () => {
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [product, setProduct] = useState({
-    name: "",
-    imageURL: "",
-    price: null,
-    category: "",
-    brand: "",
-    desc: "",
+    ...initialState,
   });
 
-  const handleInputChange = (e) => {};
-  const handleImageChange = (e) => {};
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    // console.log(e.target);
+
+    setProduct({ ...product, [name]: value });
+  };
+
+  // STEPS TO UPLOAD IMAGE ON FIREBASE
+  // https://firebase.google.com/docs/storage/web/upload-files
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    // console.log(file);
+    const storageRef = ref(storage, `eshop/${Date.now()} ${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total
+        //  number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log("Upload is " + progress + "% done");
+        setUploadProgress(progress);
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        toast.error(error.message);
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // console.log("File available at", downloadURL);
+          setProduct({ ...product, imageURL: downloadURL });
+          toast.success("Image uploaded successfully...");
+        });
+      }
+    );
+  };
+
+  // submit product to firebase
+  const addProductHandler = (e) => {
+    e.preventDefault();
+    // console.log(product);
+    setIsLoading(true);
+
+    try {
+      const docRef = addDoc(collection(db, "products"), {
+        name: product.name,
+        imageURL: product.imageURL,
+        price: Number(product.price),
+        category: product.category,
+        brand: product.brand,
+        desc: product.desc,
+        createdAt: Timestamp.now().toDate(),
+      });
+      setIsLoading(false);
+      setUploadProgress(0);
+      setProduct({ ...initialState });
+      toast.success("Products uploaded successfully...");
+
+      navigate("/admin/all-products");
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
 
   return (
-    <div className={styles.product}>
-      <h1>Add New Product</h1>
+    <>
+      {isLoading && <Loader />}
 
-      <Card cardClass={styles.card}>
-        <form>
-          <label>Product Name:</label>
-          <input
-            type="text"
-            placeholder="Product Name"
-            required
-            name="name"
-            value={product.name}
-            onChange={(e) => handleInputChange(e)}
-          />
+      <div className={styles.product}>
+        <h1>Add New Product</h1>
 
-          <label>Product Image:</label>
-          <Card cardClass={styles.group}>
-            <div className={styles.progress}>
-              <div className={styles["progress-bar"]} style={{ width: "30%" }}>
-                Uploading 30%
-              </div>
-            </div>
-            <input
-              type="file"
-              placeholder="Product image"
-              accept="image/*"
-              name="image"
-              onChange={(e) => handleImageChange(e)}
-            />
+        <Card cardClass={styles.card}>
+          <form onSubmit={addProductHandler}>
+            <label>Product Name:</label>
             <input
               type="text"
+              placeholder="Product Name"
               required
-              name="imageURL"
-              value={product.imageURL}
+              name="name"
+              value={product.name}
               onChange={(e) => handleInputChange(e)}
-              disabled
             />
-          </Card>
 
-          <label>Product Price:</label>
-          <input
-            type="number"
-            placeholder="Product Price"
-            required
-            name="price"
-            value={product.price}
-            onChange={(e) => handleInputChange(e)}
-          />
+            <label>Product Image:</label>
+            <Card cardClass={styles.group}>
+              {uploadProgress === 0 ? null : (
+                <div className={styles.progress}>
+                  <div
+                    className={styles["progress-bar"]}
+                    style={{ width: `${uploadProgress}%` }}
+                  >
+                    {uploadProgress < 100
+                      ? `Uploading ${uploadProgress}`
+                      : `Upload Completed ${uploadProgress}%`}
+                  </div>
+                </div>
+              )}
 
-          <label>Product Category:</label>
-          <select
-            name="category"
-            required
-            value={product.category}
-            onChange={(e) => handleInputChange(e)}
-          >
-            <option value="" disabled>
-              --Choose Product Category--
-            </option>
-            {categories.map((category) => {
-              return (
-                <option key={category.id} value={category.name}>
-                  {category.name}
-                </option>
-              );
-            })}
-          </select>
+              <input
+                type="file"
+                placeholder="Product image"
+                accept="image/*"
+                name="image"
+                onChange={(e) => handleImageChange(e)}
+              />
 
-          <label>Product Company / Brand:</label>
-          <input
-            type="text"
-            name="brand"
-            placeholder="Product brand"
-            required
-            value={product.brand}
-            onChange={(e) => handleInputChange(e)}
-          />
+              {product.imageURL === "" ? null : (
+                <input
+                  type="text"
+                  // required
+                  placeholder="Image url"
+                  name="imageURL"
+                  value={product.imageURL}
+                  onChange={(e) => handleInputChange(e)}
+                  disabled
+                />
+              )}
+            </Card>
 
-          <label>Product Description: </label>
-          <textarea
-            name="desc"
-            required
-            value={product.desc}
-            onChange={(e) => handleInputChange(e)}
-            cols="30"
-            rows="10"
-          ></textarea>
+            <label>Product Price:</label>
+            <input
+              type="number"
+              placeholder="Product Price"
+              required
+              name="price"
+              value={product.price}
+              onChange={(e) => handleInputChange(e)}
+            />
 
-          <button type="submit" className="--btn --btn-primary">
-            Save Product
-          </button>
-        </form>
-      </Card>
-    </div>
+            <label>Product Category:</label>
+            <select
+              name="category"
+              required
+              value={product.category}
+              onChange={(e) => handleInputChange(e)}
+            >
+              <option value="" disabled>
+                --Choose Product Category--
+              </option>
+              {categories.map((category) => {
+                return (
+                  <option key={category.id} value={category.name}>
+                    {category.name}
+                  </option>
+                );
+              })}
+            </select>
+
+            <label>Product Company / Brand:</label>
+            <input
+              type="text"
+              name="brand"
+              placeholder="Product brand"
+              required
+              value={product.brand}
+              onChange={(e) => handleInputChange(e)}
+            />
+
+            <label>Product Description: </label>
+            <textarea
+              name="desc"
+              required
+              value={product.desc}
+              onChange={(e) => handleInputChange(e)}
+              cols="30"
+              rows="10"
+            ></textarea>
+
+            <button type="submit" className="--btn --btn-primary">
+              Save Product
+            </button>
+          </form>
+        </Card>
+      </div>
+    </>
   );
 };
 
--- src/components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/components/adminOnlyRoute/AdminOnlyRoute.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectEmail } from "../../redux/slice/authSlice";
+import { selectEmail, selectIsLoggedIn } from "../../redux/slice/authSlice";
 import { Link } from "react-router-dom";
+import Loader from "../loader/Loader";
 
 const AdminOnlyRoute = ({ children }) => {
   const userEmail = useSelector(selectEmail);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  if (userEmail === "gyan.adms@gmail.com") {
+  if (userEmail === "gyan.mu@gmail.com") {
     return children;
   }
   return (
-    <section style={{ height: "82vh" }}>
-      <div className="container">
-        <h2>Permission Denied.</h2>
-        <p>This page can only be view by an Admin Users.</p>
-        <br />
-        <Link to="/">
-          <button className="--btn"> &larr; Back to Home</button>
-        </Link>
-      </div>
-    </section>
+    <>
+      {isLoggedIn === false ? (
+        <Loader />
+      ) : (
+        <section style={{ height: "82vh" }}>
+          <div className="container">
+            <h2>Permission Denied.</h2>
+            <p>This page can only be view by an Admin Users.</p>
+            <br />
+            <Link to="/">
+              <button className="--btn"> &larr; Back to Home</button>
+            </Link>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
@@ -28,7 +36,7 @@ export default AdminOnlyRoute;
 export const AdminOnlyLink = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "gyan.adms@gmail.com") {
+  if (userEmail === "gyan.mu@gmail.com") {
     return children;
   }
   return null;
