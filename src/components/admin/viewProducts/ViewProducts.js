bacab0f6af5bd770f6d6ee7dd88d25b77b5e4925
Huski-commando <gyan.mu@gmail.com>
2023-08-08 12:51:00 +0530

    working on storing products in redux store

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,8 +1,160 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ViewProducts.module.scss";
+import { toast } from "react-toastify";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db, storage } from "../../../firebase/config";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Loader from "../../loader/Loader";
+import { deleteObject, ref } from "firebase/storage";
+import Notiflix from "notiflix";
+import { useDispatch } from "react-redux";
+import { STORE_PRODUCTS } from "../../../redux/slice/productSlice";
 
 const ViewProducts = () => {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  // fetching list of products from firebase
+  const getProduct = () => {
+    setIsLoading(true);
+
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // console.log(allProducts);
+        setProducts(allProducts);
+        setIsLoading(false);
+
+        // store products to redux store
+        dispatch(
+          STORE_PRODUCTS({
+            products: allProducts,
+          })
+        );
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      "Delete Product!!!",
+      "You are about to delete this product ?",
+      "Delete",
+      "Cancel",
+      function okCb() {
+        deleteProductHandler(id, imageURL);
+      },
+      function cancelCb() {
+        console.log("delete canceled");
+      },
+      {
+        width: "320px",
+        borderRadius: "3px",
+        titleColor: "orangered",
+        okButtonBackground: "orangered",
+        cssAnimationStyle: "zoom",
+        // etc...
+      }
+    );
+  };
+
+  const deleteProductHandler = async (id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+
+      // Create a reference to the file to delete
+      const storageRef = ref(storage, imageURL);
+
+      // Delete the file
+      await deleteObject(storageRef);
+      toast.success("Product deleted successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      {isLoading && <Loader />}
+
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {products.length === 0 ? (
+          <p>No Products found...</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                // console.log(product);
+                const { id, name, imageURL, category, price } = product;
+
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>${price}</td>
+                    <td className={styles.icons}>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                        &nbsp;
+                      </Link>
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                        onClick={() => confirmDelete(id, imageURL)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ViewProducts;
